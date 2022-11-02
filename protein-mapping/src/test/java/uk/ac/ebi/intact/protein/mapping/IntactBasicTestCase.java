4bcc952c4e1fb30d4b86a5882454ee1b8561a07e
jmedinaebi <jmedina@ebi.ac.uk>
2022-11-02 14:57:27 +0000

    Remove Javadoc

--- protein-mapping/src/test/java/uk/ac/ebi/intact/protein/mapping/IntactBasicTestCase.java
+++ protein-mapping/src/test/java/uk/ac/ebi/intact/protein/mapping/IntactBasicTestCase.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2001-2007 The European Bioinformatics Institute.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package uk.ac.ebi.intact.protein.mapping;
 
 import org.junit.After;
@@ -33,12 +18,6 @@ import uk.ac.ebi.intact.core.persister.CorePersister;
 import uk.ac.ebi.intact.core.persister.PersisterHelper;
 import uk.ac.ebi.intact.core.unit.IntactMockBuilder;
 
-/**
- * Base for all intact-tests.
- *
- * @author Bruno Aranda (baranda@ebi.ac.uk)
- * @version $Id$
- */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath*:/META-INF/jpa.test.spring.xml" })
 @Transactional("transactionManager")
